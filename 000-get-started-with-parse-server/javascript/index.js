b17a232aa56b30cd2cbca93025dcac923d466d48
Andrianarisoa Daniel <radandevist@gmail.com>
2024-10-05 10:05:20 +0300

    get started parse server

---
+++ 000-get-started-with-parse-server/javascript/index.js
@@ -0,0 +1,29 @@
+const ParseServer = require('parse-server').default
+const express = require('express')
+
+const main = async () => {
+  try {
+    const app = express();
+    
+    const parseServer = new ParseServer({
+      appId: 'my-app-id',
+      masterKey: 'my-master-key',
+      databaseURI: 'mongodb://localhost:27017/my-database-name',
+      cloud: './cloud.js',
+      publicServerURL: 'http://localhost:3000/parse',
+      serverURL: 'http://localhost:3000/parse',
+    });
+    
+    await parseServer.start();
+    
+    app.use('/parse',parseServer.app);
+    
+    app.listen(3000, () => {
+      console.log('running on port 3000')
+    });
+  } catch (error) {
+  console.error(error)
+  }
+};
+
+main();
